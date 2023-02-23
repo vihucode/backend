6313d1f7622c934a240f68ee03cef45e5b30bf01
Vili Huotari <vili.huotari@gmail.com>
2023-02-23 18:04:47 +0200

    Added mongo.js and added mongo to indexx.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 let persons = [
     {
@@ -34,48 +36,45 @@ app.get('/info', (req, res) => {
     res.send('<p>Phonebook has info for ' + persons.length + ' people </p> ' + now.toString())
   })
   
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
+})
 
-app.get('/api/persons:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-    
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
   })
+})
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
-    const person = request.body
-    if (persons.find(item => item.name === person.name)) {
-        return response.status(400).json({ 
-          error: 'name must be unique' 
-          })
-    }else if(persons.find(item => item.number === person.number)){
-        return response.status(400).json({ 
-            error: 'number must be unique' 
-          })
-    }else{
-        person.id = Math.floor(Math.random() * 10000);
-        persons = persons.concat(person)
-        response.json(person)
-    }
+  const body = request.body
 
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'name missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
   })
 
-const PORT = process.env.PORT || 3001
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
